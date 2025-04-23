b6887109bee06b51ae3bc39314f9f657495da168
martshaw <info@martshaw.com>
2025-04-23 15:40:32 +0100

    updating the favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -119,10 +119,10 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
 
         {/* Favicon set */}
-        <link rel="icon" href="/thinking-emoji.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/thinking-emoji-180.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/thinking-emoji-32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/thinking-emoji-16.png" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
       </head>
